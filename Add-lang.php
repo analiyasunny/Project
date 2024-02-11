319d25fb054460daaba5403459c62e316b99ff47
analiyasunny <analiyasunny06092004@gmail.com>
2024-02-11 01:33:28 -0500

    Creating new page

---
+++ Add-lang.php
@@ -0,0 +1,27 @@
+<?php 
+    include('shared/header.php'); 
+    $title = 'World Languages and Speakers';
+?>
+ <h2>World Languages and Speakers</h2>
+    <form method="post" action="insert-lang.php">
+        <fieldset>
+            <label for="Language">Language: *</label>
+            <input name="Language" id="Language" required/>
+        </fieldset>
+        <fieldset>
+            <label for="NativeSpeakers">Native Speakers: *</label>
+            <input name="NativeSpeakers" id="NativeSpeakers" required/>
+        </fieldset>
+        <fieldset>
+            <label for="Country">Country: *</label>
+            <input name="Country" id="Country" required/> 
+        </fieldset>
+        <fieldset>
+            <label for="linguisticage">Linguistic age: *</label>
+            <input name="linguisticage" id="linguisticage" required>
+           
+        </fieldset>
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
---
+++ Shared/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Language Table</h1>
+    <ul>
+        <li>
+            <a href="Home.php">Home</a>
+        </li>
+        <li>
+            <a href="Add-lang.php">Add Language</a>
+        </li>
+        <li>
+            <a href="Lang-table.php">Language Table</a>
+        </li>
+    </ul>
+    </body>
+</html>
